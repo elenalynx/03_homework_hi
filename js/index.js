367279ad8712257a3c08e2c4b2e43a26a4423867
elenalynx <elnlynx@gmail.com>
2022-10-06 01:20:56 +0300

    added index.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const question = "Please, enter the number";
+let result;
+const number1 = Number(prompt(`${question} 1`));
+const number2 = Number(prompt(`${question} 2`));
+result = number1 + number2;
+alert(`Sum: ${result}`);
